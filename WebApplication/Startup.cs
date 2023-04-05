bbc935a78fb9ca50e066b4df613c812fbc7c2501
Mikhail Nazarov <nazarov23892@gmail.com>
2023-04-05 20:08:56 +0600

    setup ef db context

--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -7,15 +7,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataLayer.DataContexts;
+using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
 
 namespace WebApplication
 {
     public class Startup
     {
+        private IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString = configuration.GetConnectionString("BookApp_Identity");
+            services.AddDbContext<AppIdentityDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString: connectionString);
+            });
+            services.AddIdentity<AppUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppIdentityDbContext>();
+
             services.AddMvc();
         }
 
