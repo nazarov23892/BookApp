02661c78db331e8601bb296dc95cec5e47408d9b
Mikhail Nazarov <nazarov23892@gmail.com>
2023-04-05 19:56:09 +0600

    added Ef Db Contetxt

---
+++ DataLayer/DataContexts/AppIdentityDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using DataLayer.Entities;
+
+namespace DataLayer.DataContexts
+{
+    public class AppIdentityDbContext: IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
