ada056e733860b2d1f441ecc79fc1a8aa96ddd4f
Mikhail Nazarov <nazarov23892@gmail.com>
2023-04-07 15:33:09 +0600

    added cart service tests

--- BookApp.Tests/CartServiceTests.cs
+++ BookApp.Tests/CartServiceTests.cs
@@ -180,5 +180,100 @@ namespace BookApp.Tests
             Assert.Equal(line2.Quantity, lines[0].Quantity);
             Assert.Equal(line1.Quantity, lines[1].Quantity);
         }
+
+        [Fact]
+        public void Test_Remove()
+        {
+            var book1 = new BookForCartDto
+            {
+                BookId = new Guid("00000000-0000-0000-0000-000000000001"),
+                Title = "book-1",
+                Price = 1.1M
+            };
+            var book2 = new BookForCartDto
+            {
+                BookId = new Guid("00000000-0000-0000-0000-000000000002"),
+                Title = "book-2",
+                Price = 2.2M
+            };
+            var book3 = new BookForCartDto
+            {
+                BookId = new Guid("00000000-0000-0000-0000-000000000003"),
+                Title = "book-3",
+                Price = 3.3M
+            };
+            Mock<ICartLinesSessionSaver> mock = new Mock<ICartLinesSessionSaver>();
+            mock.Setup(m => m.Read()).Returns(() =>
+            {
+                return new[]
+                {
+                    new CartLine { Book = book1, Quantity = 11 },
+                    new CartLine { Book = book2, Quantity = 12 },
+                    new CartLine { Book = book3, Quantity = 13 }
+                };
+            });
+
+            SessionCartService target = new SessionCartService(mock.Object);
+
+            target.Remove(book2);
+            CartLine[] linesAfterRemoveBook2 = target.Lines
+                .ToArray();
+
+            target.Remove(book3);
+            CartLine[] linesAfterRemoveBook3 = target.Lines
+                .ToArray();
+
+            target.Remove(book1);
+            CartLine[] linesAfterRemoveBook1 = target.Lines
+                .ToArray();
+
+            Assert.Equal(2, linesAfterRemoveBook2.Length);
+            Assert.Equal(book1.BookId, linesAfterRemoveBook2[0].Book.BookId);
+            Assert.Equal(book3.BookId, linesAfterRemoveBook2[1].Book.BookId);
+            Assert.Equal(11, linesAfterRemoveBook2[0].Quantity);
+            Assert.Equal(13, linesAfterRemoveBook2[1].Quantity);
+
+            Assert.Single(linesAfterRemoveBook3);
+            Assert.Equal(book1.BookId, linesAfterRemoveBook2[0].Book.BookId);
+            Assert.Equal(11, linesAfterRemoveBook2[0].Quantity);
+
+            Assert.False(linesAfterRemoveBook1.Any());
+        }
+
+        [Fact]
+        public void Test_Clear()
+        {
+            var line1 = new CartLine
+            {
+                Book = new BookForCartDto
+                {
+                    BookId = new Guid("00000000-0000-0000-0000-000000000001"),
+                    Title = "book-1",
+                    Price = 1.1M
+                },
+                Quantity = 1
+            };
+            var line2 = new CartLine
+            {
+                Book = new BookForCartDto
+                {
+                    BookId = new Guid("00000000-0000-0000-0000-000000000002"),
+                    Title = "book-2",
+                    Price = 2.2M
+                },
+                Quantity = 2
+            };
+            Mock<ICartLinesSessionSaver> mock = new Mock<ICartLinesSessionSaver>();
+            mock.Setup(m => m.Read()).Returns(() =>
+            {
+                return new[] { line1, line2 };
+            });
+
+            SessionCartService target = new SessionCartService(saver: mock.Object);
+
+            target.Clear();
+
+            Assert.False(target.Lines.Any());
+        }
     }
 }
