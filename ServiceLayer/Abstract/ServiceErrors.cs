b3625c560826396661592a2eb5e3b79dd1d4da7f
Mikhail Nazarov <nazarov23892@gmail.com>
2023-04-09 09:01:01 +0600

    added base classes for errors notifying logic

---
+++ ServiceLayer/Abstract/ServiceErrors.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ServiceLayer.Abstract
+{
+    public abstract class ServiceErrors
+    {
+        private readonly List<ValidationResult> errors = new List<ValidationResult>();
+        public IEnumerable<ValidationResult> Errors 
+        {
+            get => errors;
+        }
+
+        public bool HasErrors 
+        {
+            get => errors.Any();
+        }
+
+        protected void AddError(string errorMessage, params string[] memberNames)
+        {
+            errors.Add(new ValidationResult(
+                errorMessage: errorMessage,
+                memberNames: memberNames));
+        }
+    }
+}
