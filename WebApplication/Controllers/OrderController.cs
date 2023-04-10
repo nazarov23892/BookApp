2abd1b07b5c1e66f585b5fb5c60c5b168920d953
Mikhail Nazarov <nazarov23892@gmail.com>
2023-04-10 14:23:10 +0600

    added 'order check out' view and controller

---
+++ WebApplication/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ServiceLayer.CartServices;
+
+namespace WebApplication.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly  ICartService cartService;
+
+        public OrderController(ICartService cartService)
+        {
+            this.cartService = cartService;
+        }
+
+        public ViewResult Checkout()
+        {
+            return View(model: cartService.Lines);
+        }
+    }
+}
