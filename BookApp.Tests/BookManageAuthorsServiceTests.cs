73a124b07a8a23c35ec3d14489a251544432de99
Mikhail Nazarov <nazarov23892@gmail.com>
2023-05-02 16:12:11 +0600

    BookManageAuthors service: test added

--- BookApp.Tests/BookManageAuthorsServiceTests.cs
+++ BookApp.Tests/BookManageAuthorsServiceTests.cs
@@ -99,13 +99,68 @@ namespace BookApp.Tests
                 {
                     AuthorId = new Guid("00000000-0000-0000-0000-000000000002"),
                     BookId = new Guid("00000000-0000-0000-0000-000000000051")
-                }) ;
+                });
 
             // Arrange
             Assert.True(target.HasErrors);
             Assert.Single(collection: target.Errors);
             Assert.Contains(
                 expectedSubstring: "author not found",
+                actualString: target.Errors.Single().ErrorMessage);
+            mock.Verify(
+                expression: m => m.SaveBook(It.IsAny<Book>()),
+                times: Times.Never);
+        }
+
+        [Fact]
+        public void Cannot_Add_Author_When_Author_Is_Already_Added()
+        {
+            // Arrange
+            Mock<IBookManageAuthorsDbAccess> mock = new Mock<IBookManageAuthorsDbAccess>();
+            mock.Setup(m => m.GetAuthor(It.IsAny<Guid>()))
+                .Returns<Guid>(authorId =>
+                {
+                    var author = new Author
+                    {
+                        AuthorId = new Guid("00000000-0000-0000-0000-000000000001")
+                    };
+                    return authorId == author.AuthorId
+                        ? author
+                        : null;
+                });
+            mock.Setup(m => m.GetBookWithAuthorLinks(It.IsAny<Guid>()))
+                .Returns<Guid>(bookId =>
+                {
+                    var book = new Book
+                    {
+                        BookId = new Guid("00000000-0000-0000-0000-000000000051"),
+                        AuthorsLink = new[]
+                        {
+                            new BookAuthor
+                            {
+                                AuthorId = new Guid("00000000-0000-0000-0000-000000000001") 
+                            }
+                        }.ToList()
+                    };
+                    return bookId == book.BookId
+                        ? book
+                        : null;
+                });
+            BookManageAuthorsService target = new BookManageAuthorsService(mock.Object);
+
+            // Act
+            target.AddAuthor(
+                addAuthorDto: new BookAddAuthorDto
+                {
+                    AuthorId = new Guid("00000000-0000-0000-0000-000000000001"),
+                    BookId = new Guid("00000000-0000-0000-0000-000000000051")
+                });
+
+            // Arrange
+            Assert.True(target.HasErrors);
+            Assert.Single(collection: target.Errors);
+            Assert.Contains(
+                expectedSubstring: "book already contains given author",
                 actualString: target.Errors.Single().ErrorMessage);
             mock.Verify(
                 expression: m => m.SaveBook(It.IsAny<Book>()),
