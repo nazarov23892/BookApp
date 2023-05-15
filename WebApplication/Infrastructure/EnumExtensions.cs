57f7bc947da617d0df08023264a026be92bf385f
Mikhail Nazarov <nazarov23892@gmail.com>
2023-05-15 14:36:20 +0600

    book catalog: fixed displaying sort values in filter panel

---
+++ WebApplication/Infrastructure/EnumExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace WebApplication.Infrastructure
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            return enumValue.GetType()
+                            .GetMember(enumValue.ToString())
+                            .First()
+                            .GetCustomAttribute<DisplayAttribute>()
+                            .GetName();
+        }
+    }
+}
