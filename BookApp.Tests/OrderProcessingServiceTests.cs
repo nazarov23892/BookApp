0d91f9bebfdc25e0a83ddc4261f3c1210ac7f4a2
Mikhail Nazarov <nazarov23892@gmail.com>
2023-05-15 08:48:19 +0600

    OrderProcessing service: added tests

--- BookApp.Tests/OrderProcessingServiceTests.cs
+++ BookApp.Tests/OrderProcessingServiceTests.cs
@@ -504,5 +504,165 @@ namespace BookApp.Tests
                     )),
                 times: Times.Once);
         }
+
+        [Fact]
+        public void Cannot_Goto_Completed_Status_From_Noncorrespond_Status()
+        {
+            // arrange
+            Book book1 = new Book { BookId = new Guid("00000000-0000-0000-0000-000000000001") };
+            var order1 = new Order
+            {
+                OrderId = 1,
+                Status = OrderStatus.New,
+                Lines = new[] { new OrderLineItem { BookId = book1.BookId } }
+            };
+            var order2 = new Order
+            {
+                OrderId = 2,
+                Status = OrderStatus.Assembling,
+                Lines = new[] { new OrderLineItem { BookId = book1.BookId } }
+            };
+            var order3 = new Order
+            {
+                OrderId = 3,
+                Status = OrderStatus.Cancelled,
+                Lines = new[] { new OrderLineItem { BookId = book1.BookId } }
+            };
+            var order4 = new Order
+            {
+                OrderId = 4,
+                Status = OrderStatus.Completed,
+                Lines = new[] { new OrderLineItem { BookId = book1.BookId } }
+            };
+
+            // arrange
+            var ordersDict = new[] { order1, order2, order3, order4 }
+                .ToDictionary(o => o.OrderId);
+            Mock<IOrderProcessingDbAccess> mock = new Mock<IOrderProcessingDbAccess>();
+            mock.Setup(m => m.GetOrderOrigin(It.IsAny<int>()))
+                .Returns<int>(orderId => ordersDict.ContainsKey(orderId)
+                    ? ordersDict[orderId]
+                    : null);
+
+            var target1 = new OrderProcessingService(orderProcessingDbAccess: mock.Object);
+            var target2 = new OrderProcessingService(orderProcessingDbAccess: mock.Object);
+            var target3 = new OrderProcessingService(orderProcessingDbAccess: mock.Object);
+            var target4 = new OrderProcessingService(orderProcessingDbAccess: mock.Object);
+
+            // act
+            target1.SetOrderStatusToCompleted(orderId: order1.OrderId);
+            target2.SetOrderStatusToCompleted(orderId: order2.OrderId);
+            target3.SetOrderStatusToCompleted(orderId: order3.OrderId);
+            target4.SetOrderStatusToCompleted(orderId: order4.OrderId);
+
+            // assert
+            Assert.True(target1.HasErrors);
+            Assert.True(target2.HasErrors);
+            Assert.True(target3.HasErrors);
+            Assert.True(target4.HasErrors);
+
+            Assert.Single(target1.Errors);
+            Assert.Single(target2.Errors);
+            Assert.Single(target3.Errors);
+            Assert.Single(target4.Errors);
+
+            Assert.Contains(
+                expectedSubstring: "can complete ready orders only",
+                actualString: target1.Errors.Single().ErrorMessage);
+            Assert.Contains(
+                expectedSubstring: "can complete ready orders only",
+                actualString: target2.Errors.Single().ErrorMessage);
+            Assert.Contains(
+                expectedSubstring: "can complete ready orders only",
+                actualString: target3.Errors.Single().ErrorMessage);
+            Assert.Contains(
+                expectedSubstring: "can complete ready orders only",
+                actualString: target4.Errors.Single().ErrorMessage);
+
+            mock.Verify(
+                expression: m => m.SaveOrder(It.IsAny<Order>()),
+                times: Times.Never);
+        }
+
+        [Fact]
+        public void Cannot_Goto_Completed_Status_When_Order_Not_Exist_In_Db()
+        {
+            // arrange
+            Book book1 = new Book { BookId = new Guid("00000000-0000-0000-0000-000000000001") };
+            var ordersDict = new[]
+            {
+                new Order
+                {
+                    OrderId = 1, Status = OrderStatus.New,
+                    Lines = new [] { new OrderLineItem { BookId = book1.BookId } }
+                }
+            }
+            .ToDictionary(o => o.OrderId);
+
+            Mock<IOrderProcessingDbAccess> mock = new Mock<IOrderProcessingDbAccess>();
+            mock.Setup(m => m.GetOrderOrigin(It.IsAny<int>()))
+                .Returns<int>(orderId => ordersDict.ContainsKey(orderId)
+                    ? ordersDict[orderId]
+                    : null);
+
+            var target = new OrderProcessingService(orderProcessingDbAccess: mock.Object);
+
+            // act
+            target.SetOrderStatusToCompleted(orderId: 4);
+
+            // assert
+            Assert.True(target.HasErrors);
+            Assert.Single(target.Errors);
+            Assert.Contains(
+                expectedSubstring: "order not found",
+                actualString: target.Errors.Single().ErrorMessage);
+            mock.Verify(
+                expression: m => m.SaveOrder(It.IsAny<Order>()),
+                times: Times.Never);
+        }
+
+        [Fact]
+        public void Can_Goto_Completed_Status()
+        {
+            // arrange
+            Book book1 = new Book { BookId = new Guid("00000000-0000-0000-0000-000000000001") };
+            Order order1 = new Order
+            {
+                OrderId = 1,
+                Status = OrderStatus.Ready,
+                Lines = new[]
+                {
+                    new OrderLineItem { BookId = book1.BookId }
+                }
+            };
+            var ordersDict = new[] { order1 }
+                .ToDictionary(o => o.OrderId);
+
+            Mock<IOrderProcessingDbAccess> mock = new Mock<IOrderProcessingDbAccess>();
+            mock.Setup(m => m.GetOrderOrigin(It.IsAny<int>()))
+                .Returns<int>(orderId => ordersDict.ContainsKey(orderId)
+                    ? ordersDict[orderId]
+                    : null);
+            mock.Setup(m => m.GetOrderLines(It.IsAny<int>()))
+                .Returns<int>(orderId => ordersDict.ContainsKey(orderId)
+                    ? ordersDict[orderId].Lines
+                    : null);
+
+            var target = new OrderProcessingService(orderProcessingDbAccess: mock.Object);
+
+            // act
+            target.SetOrderStatusToCompleted(orderId: 1);
+
+            // assert
+            Assert.False(target.HasErrors);
+            Assert.Empty(target.Errors);
+
+            mock.Verify(
+                expression: m => m.SaveOrder(It.Is<Order>(o =>
+                    o.OrderId == order1.OrderId
+                    && o.Status == OrderStatus.Completed
+                    )),
+                times: Times.Once);
+        }
     }
 }
